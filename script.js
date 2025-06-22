61d8202d7951409638a302b3f929ad8dcdcc8aa3
Ian0822 <ian08228877@gmail.com>
2025-06-22 23:59:23 +0800

    合併js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function updateTextBasedOnWidth() {
   const teamName = document.querySelector('.team-name');
   const gameTitle = document.querySelector('.game-title');
@@ -16,23 +15,4 @@ function updateTextBasedOnWidth() {
 updateTextBasedOnWidth();
 
 // 螢幕大小改變時再判斷一次
-=======
-function updateTextBasedOnWidth() {
-  const teamName = document.querySelector('.team-name');
-  const gameTitle = document.querySelector('.game-title');
-
-  if (window.innerWidth <= 1000) {
-    teamName.textContent = 'Subway';
-    gameTitle.textContent = '《FTL》';
-  } else {
-    teamName.textContent = 'Subway Team';
-    gameTitle.textContent = '《Finding The Light》';
-  }
-}
-
-// 初始載入時執行一次
-updateTextBasedOnWidth();
-
-// 螢幕大小改變時再判斷一次
->>>>>>> 47dc53b3e59d871de47576adb25083df63100854
 window.addEventListener('resize', updateTextBasedOnWidth);
